29249413fec793659a4e2112e707f3c398c8fd09
Sonia Merayo <techkrowd@gmail.com>
2020-03-24 16:58:35 +0100

    funciones de leds

--- simon/simon.ino
+++ simon/simon.ino
@@ -19,15 +19,44 @@ const int BUTTONS[] = {BTN_GREEN, BTN_RED, BTN_BLUE, BTN_YELLOW};
 
 const int SIZE = 4;
 
+const int ROUNDS = 20;
+int sequence[ROUNDS];
+int n = 0;
+
 void setup(){
   Serial.begin(9600);
-
+  randomSeed(analogRead(0));
   for(int i=0; i<SIZE; i++){
     pinMode(LEDS[i], OUTPUT);
     pinMode(BUTTONS[i], INPUT);
   } 
 }
 
+void printLed(int led){
+  digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+}
+
+void randomSequence(){
+  for (int i=0; i<ROUNDS; i++){
+    sequence[i] = random(0,SIZE);
+  }
+  n = 1;  
+}
+
+void printSequence(){
+  delay(1000);
+  for(int i=0; i<n; i++){
+    printLed(LEDS[sequence[i]]);
+    Serial.println(sequence[i]);
+    delay(1000);
+  }
+}
+
 void loop(){
-  
+  Serial.println("Nueva secuencia: ");
+  randomSequence();
+  n=5;
+  printSequence();
 }
